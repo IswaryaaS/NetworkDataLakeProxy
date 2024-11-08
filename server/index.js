b95170dc97c5244758f2fb3c7786828c4f14c206
Martin Fuchs <m.fuchs@mentopolis.de>
2024-11-08 10:51:57 +0100

    add call to appCommons.performApplicationRegistration();

--- server/index.js
+++ server/index.js
@@ -32,3 +32,8 @@ http.createServer(app).listen(serverPort, function () {
     console.log('Your server is listening on port %d (http://localhost:%d)', serverPort, serverPort);
     console.log('Swagger-ui is available on http://localhost:%d/docs', serverPort);
 });
+
+// perform application registration
+appCommons.performApplicationRegistration();
+
+logger.info("NetworkDataLakeProxy is up.");
